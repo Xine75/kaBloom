48b74dc86205031166c1a3a59fd3a98c6f4d194b
Xine75 <cjdoza@gmail.com>
2021-01-28 15:42:21 -0600

    file names

---
+++ src/components/kabloom.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { ApplicationViews } from "./ApplicationViews";
+import { Login } from "./auth/Login";
+import { Register } from "./auth/Register";
+import "./Kabloom.css";
+
+export const Kabloom = () => (
+  <>
+    <Route
+      render={() => {
+        if (localStorage.getItem("kabloom_user")) {
+          return (
+            <>
+              <ApplicationViews />
+            </>
+          );
+        } else {
+          return <Redirect to="/login" />;
+        }
+      }}
+    />
+
+    <Route path="/login">
+      <Login />
+    </Route>
+    <Route path="/register">
+      <Register />
+    </Route>
+  </>
+);
