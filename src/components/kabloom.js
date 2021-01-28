fa316285dd6127ac9d9734804bf55cc13734f116
Xine75 <cjdoza@gmail.com>
2021-01-28 11:12:01 -0600

    initial files

--- src/components/kabloom.js
+++ src/components/kabloom.js
@@ -1,19 +1,17 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import { ApplicationViews } from "./ApplicationViews";
-import { NavBar } from "./nav/NavBar";
 import { Login } from "./auth/Login";
 import { Register } from "./auth/Register";
-import "./Kennel.css";
+import "./kabloom.css";
 
-export const Kennel = () => (
+export const kabloom = () => (
   <>
     <Route
       render={() => {
-        if (localStorage.getItem("kennel_customer")) {
+        if (localStorage.getItem("kabloom_user")) {
           return (
             <>
-              <NavBar />
               <ApplicationViews />
             </>
           );
