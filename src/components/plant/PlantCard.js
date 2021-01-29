43d513cad6d080d5d4f07db171ddd1a959e50500
Xine75 <cjdoza@gmail.com>
2021-01-29 16:34:09 -0600

    friday

--- src/components/plant/PlantCard.js
+++ src/components/plant/PlantCard.js
@@ -7,25 +7,29 @@ import "./Plant.css"
 //3. "Last Watered" text field will update to DOM
 //4. Rendering a "More" button that will take user to notes for a specific plant
 
-// const history = useHistory()
-
 export const PlantCard = ({ plant }) => {
     {console.log("plant", plant)}
-
+    const history = useHistory()
+    const timestamp = Date.now()
+    
     return (
 
     <section className="plant">
         <img src={plant.imageURL} className="plant__image"/>
         <h3 className="plant__name">{plant.name}</h3>
         <div className="plant_type">{plant.type}</div>
-        <div className="plant_lastWatered">{plant.lastWatered}</div>
-        {/* <button className = "toNoteListBtn" onClick={() => history.push("/note/NoteList")}>
+        <div className="plant_lastWatered">Last watered: {plant.lastWatered}</div>
+        <button className = "toNoteListBtn" onClick={() => history.push("/note/NoteList")}>
           More
-      </button> */}
-        <button className = "plant__wateredToday" onClick={() => plant.lastWatered = new Date(plant.timestamp).toLocaleDateString('en-US')}>
+      </button>
+        <button className = "plant__wateredToday" onClick={() => plant.lastWatered = new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit'}).format(timestamp)}>
           Watered Today!
       </button>
 
+      {console.log("plant.lastWatered", plant.lastWatered)}
+      
     </section> 
+  
 
     )}
+
