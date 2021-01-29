8c07a313f5aabbd9bf118f68d461bc725e922fcb
Xine75 <cjdoza@gmail.com>
2021-01-29 11:34:51 -0600

    plantlist

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,14 +2,36 @@ import React from "react"
 import { Route } from "react-router-dom"
 import { Home } from "./Home"
 
+import { PlantProvider } from "./plant/PlantProvider"
+import { PlantList } from "./plant/PlantList"
+import { PlantForm } from "./plant/PlantForm"
+
+import { NoteProvider } from "./note/NoteProvider"
+
 export const ApplicationViews = () => {
     return (
         <>
-   {/* Render the location list when http://localhost:3000/ */}
-            <Route exact path="/">
-                <Home />
-            </Route>
+            {/* Render the Plant List list when http://localhost:3000/ */}
+            <PlantProvider>
+                <Route exact path="/">
+                    <PlantList />
+                </Route>
+            </PlantProvider>
+
+            <NoteProvider>
+                <PlantProvider>
+                    <Route exact path="/plant/create">
+                        <PlantForm />
+                    </Route>
+                </PlantProvider>
+            </NoteProvider>
+
+
+
+
         </>
+
+
 
     )
 }
---
+++ src/components/plant/PlantList.js
@@ -0,0 +1,38 @@
+import React, { useContext, useEffect } from "react"
+import { useHistory } from "react-router-dom"
+import { PlantContext } from "./PlantProvider"
+import { PlantCard } from "./PlantCard"
+import "./Plant.css"
+
+//Create and export PlantList, which will be responsible for:
+//1. Rendering current user's plant to the DOM
+//2. Rendering an "I got a new plant" button that will take user to PlantForm
+//3. This will act as user's home page and will need a scroll bar in CSS
+
+export const PlantList = () => {
+    const { plants, getPlants } = useContext(PlantContext)
+    const currentUser = parseInt(localStorage.getItem("kabloom_user"))
+
+    useEffect(() => {
+        getPlants()
+    }, [])
+
+    return (
+        <>
+            <div className="plants">
+                {/*map over plants and grab only those belonging to current user*/}
+                {
+                    plants.map(plant => {
+                        const userPlants = plants.filter(p => p.userId === currentUser)
+                        return <PlantCard key={plant.id}
+                            plant={userPlants}
+                        />
+                    })
+                }
+       <button onClick={() => {history.push("/plant/create")}}>
+          kaBloom! I got a new plant!
+      </button>           
+            </div>
+        </>
+    )
+}
