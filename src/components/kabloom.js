c9a9eaafdb7c8f3d0dd6f35362d575c4ae3f77e9
Xine75 <cjdoza@gmail.com>
2021-02-08 10:29:39 -0600

    centered heading

--- src/components/kabloom.js
+++ src/components/kabloom.js
@@ -3,6 +3,9 @@ import { Route, Redirect, Link } from "react-router-dom";
 import { ApplicationViews } from "./ApplicationViews";
 import { Login } from "./auth/Login";
 import { Register } from "./auth/Register";
+import Card from "react-bootstrap/Card"
+import Col from "react-bootstrap/Col"
+import Row from "react-bootstrap/Row"
 import "./Kabloom.css";
 
 export const Kabloom = () => (
@@ -12,7 +15,11 @@ export const Kabloom = () => (
         if (sessionStorage.getItem("kabloom_user")) {
           return (
             <>
-              <Link to={"/plants"} className="heading">kaBloom</Link>
+            <Row>
+              <Col></Col>
+              <Col><Link to={"/plants"} className="heading">kaBloom</Link></Col>
+              <Col></Col>
+              </Row>
               <ApplicationViews />
             </>
           );
