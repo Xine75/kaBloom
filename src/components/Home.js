b293a14b636591152925e7576313cae8b245ac92
Xine75 <cjdoza@gmail.com>
2021-02-08 19:08:34 -0600

    welcome background

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,23 @@
 import React from "react";
 import { useHistory } from "react-router-dom"
 import Button from "react-bootstrap/Button"
+import Card from "react-bootstrap/Card"
+import "./Home.css"
 
 export const Home = () => {
     const history = useHistory()
     return (
     <>
-    <h2>Welcome to kaBloom!</h2>
-    <div>Manage all your plant care needs</div>
-    <Button className="newPlant" variant="success" size="lg" style={{ width: '20rem'  }} onClick={() => {history.push("/plant/create")}}>
+   <body className="welcome__body">
+    <Card className="welcome__card">
+    <Card.Body><h2>Welcome to kaBloom!</h2>
+    <div>Add your first plant to see how kaBloom can help you manage all your plant care.  All in the palm of your frond, er --hand!</div>
+    </Card.Body>
+    <Button className="welcome__plant__btn" variant="success" size="lg" style={{ width: '20rem'  }} onClick={() => {history.push("/plant/create")}}>
           kaBloom! I got a new plant!
       </Button> 
+      </Card>
+      </body>
     </>
     )
 }
