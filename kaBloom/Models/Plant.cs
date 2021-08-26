7126aa752d4f8ad8cabee454071d442a13975133
Xine <cjdoza@gmail.com>
2021-08-26 15:44:02 -0500

    plant

---
+++ kaBloom/Models/Plant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kaBloom.Models
+{
+    public class Plant
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public DateTime DateAdopted { get; set; }
+        public string WaterNeeds { get; set; }
+        public string LightNeeds { get; set; }
+        public string Fertilize { get; set; }
+        public DateTime LastWatered { get; set; }
+        public string ImageUrl { get; set; }
+
+    }
+}
